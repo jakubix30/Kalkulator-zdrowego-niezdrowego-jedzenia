cd12d461a72226e1da114e401afe8062fc661348
jakubix30 <65811793+jakubix30@users.noreply.github.com>
2022-11-28 22:20:18 +0100

    Add files via upload

--- skrypt.js
+++ skrypt.js
@@ -19,12 +19,10 @@ function obliczanie()
             document.getElementById('showdiv').style.animationDuration = "1s";
             document.getElementById('showdiv').style.animationName = "animacja2";
             document.getElementById('showdiv').style.display = "block";
-            document.getElementById('wynik').innerHTML=
-                ("Musisz zjesc  "+(Math.round(formid1.ddlselect1.value / formid.ddlselect.value*10)/10) 
-                + " wybranego zdrowego produktu aby zastąpić wybranego niezdrowego.")};
+            document.getElementById('wynik').innerHTML= Math.round(formid1.ddlselect1.value / formid.ddlselect.value*10)/10
             document.getElementById('selectedzap1').innerHTML= formid4.input4.value;
             document.getElementById('musiszzjesczdrowe').innerHTML = (Math.round(formid4.input4.value / formid.ddlselect.value*10)/10);
-            document.getElementById('musiszzjescniezdrowe').innerHTML = Math.round(formid4.input4.value / formid1.ddlselect1.value*10)/10;
+            document.getElementById('musiszzjescniezdrowe').innerHTML = Math.round(formid4.input4.value / formid1.ddlselect1.value*10)/10;}
 }
 
 /*var li = 12.2352463745124353453
